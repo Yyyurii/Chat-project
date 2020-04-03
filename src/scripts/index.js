05120b5f8552c44e1be230c5a1644e7010177aff
Yyyurii <yurii.kryvko@gmail.com>
2020-04-03 21:31:21 +0300

    add click()

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,22 @@ import $ from 'jquery';
 
 $(document).ready (function onDocumentReady() {
 
+// Змінює фон при кліку
+$('.users').on('click', function(){
+    $(this).css({'backgroundColor': '#8688F0', 'color': 'white'});
+    $('.users').not(this).css({'backgroundColor': '', 'color': ''});
+  });
 
 
-
+$('input').on('input', function(){
+    switch ($(this).val()) {
+        case 'Chandler Bing':
+            $('#Chandler Bing').show();
+            $('.qw').hide();
+        // default:
+        //     $('.users').show();
+    }
+})
 
 
 
