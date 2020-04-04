f48946e63430c69621ea68bd708075b70ac2558d
Yyyurii <yurii.kryvko@gmail.com>
2020-04-04 19:51:19 +0300

    add numbering and search by users

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,52 @@
 import $ from 'jquery';
 
-$(document).ready (function onDocumentReady() {
+$(document).ready(function onDocumentReady() {
 
-// Змінює фон при кліку
-$('.users').on('click', function(){
-    $(this).css({'backgroundColor': '#8688F0', 'color': 'white'});
-    $('.users').not(this).css({'backgroundColor': '', 'color': ''});
-  });
+    // Змінює фон при кліку
+    $('.users').on('click', function () {
+        $(this).css({ 'backgroundColor': '#8688F0', 'color': 'white' });
+        $('.users').not(this).css({ 'backgroundColor': '', 'color': '' });
+    });
+
+    // Нумерація юзерів
+    $('.users-name').each(function (i) {
+        var number = i + 1 + '. ';
+        $(this).prepend(number);
+    });
+
+    // Search
+    $('input').on('input', function () {
+        var $inptuVal = $('input').val();
+        console.log($('input').val());
+        $('.users-name').each(function (index, element) {
+            var $element = $(element);
+            var $elementName = $element.text(); // names of users
+            var $parent = $element.closest('.users');
+            if ($elementName.includes($inptuVal)) {
+                $parent.show();
+            } else {
+                $parent.hide();
+            }
+
+        });
+    });
+
+    // Відправляє смс 
+
+    // var myMassage = $('.massage-window__my-massage-text');
+    // var myMassageText = myMassage.text();
+    // console.log(myMassageText);
+   
+    // $('textarea').on('textarea', function () {
+    //     var $inptuVal = $('textarea').val();
+    //     console.log($('textarea').text());
+    // });
+    
+    // myMassageText.each(function(){
+    //     (this).html($textAreaVal);
+    // });
 
 
-$('input').on('input', function(){
-    switch ($(this).val()) {
-        case 'Chandler Bing':
-            $('#Chandler Bing').show();
-            $('.qw').hide();
-        // default:
-        //     $('.users').show();
-    }
-})
 
 
 
@@ -34,4 +63,10 @@ $('input').on('input', function(){
 
 
 
-}) ;
+
+  
+
+
+
+
+});
